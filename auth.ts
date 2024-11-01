8c842fbe441b541bd887bf476c9c952229071550
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2024-11-01 08:13:21 +0530

    authorSchema Defined

--- auth.ts
+++ auth.ts
@@ -1,35 +1,6 @@
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
-
-const prisma = new PrismaClient()
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-      profile(profile) {
-        return {
-          id: profile.id.toString(),
-          name: profile.name || profile.login,
-          email: profile.email,
-          githubId: profile.id.toString(),
-          profilePicture: profile.avatar_url,
-        }
-      },
-    })
-  ],
-  callbacks: {
-    async session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id
-        session.user.githubId = user.githubId
-        session.user.profilePicture = user.profilePicture
-      }
-      return session
-    },
-  },
-})
+  providers: [GitHub],
+});

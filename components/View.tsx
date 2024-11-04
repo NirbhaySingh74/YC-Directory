3b7f4dd82ca9c9cdc40f43506d3703377ecf81b3
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2024-11-04 21:20:29 +0530

    product deatail page done

--- components/View.tsx
+++ components/View.tsx
@@ -3,11 +3,20 @@ import Ping from "./Ping";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_QUERY } from "@/lib/queries";
 import { formatViews } from "@/lib/utils";
-
+import { unstable_after as after } from "next/server";
+import { writeClient } from "@/sanity/lib/write-client";
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
+
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
   return (
     <div className="view-container">
       <div className="absolute -top-2 -right-2">

cd724cdd0fc4123f8ca3de1de3deb88050886da5
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2024-11-05 18:25:25 +0530

    markdown added

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -1,8 +1,11 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "./ui/input";
-
+import { Textarea } from "./ui/textarea";
+import MDEditor from "@uiw/react-md-editor";
 const StartupForm = () => {
+  const [error, setError] = useState<Record<string, string>>({});
+  const [pitch, setPitch] = useState("**Hello world!!**");
   return (
     <form action={() => {}} className="startup-form">
       <div>
@@ -11,10 +14,76 @@ const StartupForm = () => {
         </label>
         <Input
           id="title"
+          name="title"
           className="startup-form_input"
           required
           placeholder="Startup Title"
         />
+        {error.title && <p className="startup-form_error">{error.title}</p>}
+      </div>{" "}
+      <div>
+        <label htmlFor="description" className="startup-form_label">
+          Description
+        </label>
+        <Textarea
+          id="description"
+          name="description"
+          className="startup-form_textarea"
+          required
+          placeholder="Startup Description"
+        />
+        {error.description && (
+          <p className="startup-form_error">{error.description}</p>
+        )}
+      </div>{" "}
+      <div>
+        <label htmlFor="category" className="startup-form_label">
+          Category
+        </label>
+        <Input
+          id="category"
+          name="category"
+          className="startup-form_input"
+          required
+          placeholder="Startup Category (Tech, Health, Education, Food)"
+        />
+        {error.category && (
+          <p className="startup-form_error">{error.category}</p>
+        )}
+      </div>
+      <div>
+        <label htmlFor="link" className="startup-form_label">
+          Image URL
+        </label>
+        <Input
+          id="link"
+          name="link"
+          className="startup-form_input"
+          required
+          placeholder="Startup Image URL"
+        />
+        {error.link && <p className="startup-form_error">{error.link}</p>}
+      </div>{" "}
+      <div data-color-mode="light">
+        <label htmlFor="Pitch" className="startup-form_label">
+          Pitch
+        </label>
+        <MDEditor
+          value={pitch}
+          onChange={(value) => setPitch(value as string)}
+          id="pitch"
+          preview="edit"
+          height={300}
+          style={{ borderRadius: 20, overflow: "hidden" }}
+          textareaProps={{
+            placeholder:
+              "Briefly describe your idea and what problem it solves",
+          }}
+          previewOptions={{
+            disallowedElements: ["style"]
+          }}
+        />
+        {error.link && <p className="startup-form_error">{error.link}</p>}
       </div>
     </form>
   );
